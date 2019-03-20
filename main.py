b1d00eb9d5d3d503091dbec8ffd00da716568978
Hayden Shively <haydenshively@gmail.com>
2019-03-19 17:35:01 -0700

    Added on-the-fly threshold adjustments, lip remover, and chin remover

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from history import History
 
 INPUT_PATH = 'datasets/people/lac02122017_20_08_52_withaudio.avi'
 OUTPUT_PATH = 'results/people/lac02122017_20_08_52_withaudio'# NOTE: path should not include extension for output
-MEMORY_LENGTH = 20
-DIFFERENCE_THRESH = 10
-MOTION_THRESHOLD = 0.32
+MEMORY_LENGTH = 25
+DIFFERENCE_THRESH = 25
+MOTION_THRESHOLD = 0.23
 WHITE_THRESHOLD = 60
 
 from film import Film
@@ -31,7 +31,6 @@ def find_motion(frame, prev_ret_val, iter, MEMORY_LENGTH, DIFFERENCE_THRESH):
         difference[difference < DIFFERENCE_THRESH] = 0
 
         motion = motion + difference
-
         return [history, motion]
 
 @film.processor
@@ -47,26 +46,37 @@ def find_tongue(frame, prev_ret_val, iter, TONGUE_MASK, WHITE_THRESHOLD):
         mod[mod >= WHITE_THRESHOLD] = 255
 
         contours, hierarchy = cv2.findContours(mod, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        largest_contour = find_largest(contours)
+        largest_contour = np.asarray(find_largest(contours))[:, 0]
+        prev_ret_val[1].append(largest_contour)
 
-        tongue = np.zeros_like(gray)
-        cv2.drawContours(tongue, [largest_contour], 0, (255), -1)
         combined = gray.copy()
         cv2.drawContours(combined, [largest_contour], 0, (255), -1)
 
+        # display
         side_by_side = np.hstack((gray, combined))
-
-        cv2.imshow('result', cv2.pyrUp(side_by_side))
-        ch = cv2.waitKey(1)
-
-        largest_contour = np.asarray(largest_contour)[:, 0]
-        prev_ret_val[1].append(largest_contour)
+        imshowbig('result', side_by_side)
 
         return (side_by_side, prev_ret_val[1])
 
 
 _, motion = find_motion(MEMORY_LENGTH = MEMORY_LENGTH, DIFFERENCE_THRESH = DIFFERENCE_THRESH)
+imshowbig('motion', (motion*255/motion.max()).astype('uint8'))
+print('Choose new constants for motion masking, or type continue to move on.')
+input_mem_length = input('MEMORY LENGTH: ')
+input_dif_thresh = input('DIFFERENCE THRESH: ')
+while ('c' not in input_mem_length) and ('c' not in input_dif_thresh):
+    _, motion = find_motion(MEMORY_LENGTH = int(eval(input_mem_length)), DIFFERENCE_THRESH = eval(input_dif_thresh))
+    imshowbig('motion', (motion*255/motion.max()).astype('uint8'))
+    input_mem_length = input('MEMORY LENGTH: ')
+    input_dif_thresh = input('DIFFERENCE THRESH: ')
+
 tongue_mask = extract_tongue_from(motion, THRESHOLD = MOTION_THRESHOLD)
+print('Choose new constant for tongue extraction, or type continue to move on.')
+input_thresh = input('MOTION THRESHOLD: ')
+while 'c' not in input_thresh:
+    tongue_mask = extract_tongue_from(motion, THRESHOLD = eval(input_thresh))
+    input_thresh = input('MOTION THRESHOLD: ')
+
 _, contour_history = find_tongue(TONGUE_MASK = tongue_mask, WHITE_THRESHOLD  = WHITE_THRESHOLD)
 
 
--- util.py
+++ util.py
@@ -3,6 +3,7 @@ import cv2
 
 def imshowbig(title, image):
     cv2.imshow(title, cv2.pyrUp(image))
+    cv2.waitKey(1)
 
 def find_largest(contours):
     largest = contours[0]
@@ -16,13 +17,98 @@ def find_largest(contours):
             largest_area = area
     return largest
 
+def match_centroid(contours, target):
+    best = None
+    best_offset = None
+    for contour in contours:
+        M = cv2.moments(contour)
+        if M['m00'] == 0: continue
+        centroid = (M['m10']/M['m00'], M['m01']/M['m00'])
+        offset = (centroid[0]-target[0])**2 + (centroid[1]-target[1])**2
+        if (best is None) or (offset < best_offset):
+            best = contour
+            best_offset = offset
+    return best
+
+def remove_lips(mask, contour):
+    hull = cv2.convexHull(contour, returnPoints = False)
+
+    defects = cv2.convexityDefects(contour, hull)
+    defects_argmax = defects[:,:,3].argmax()# remove tongue-arch defect from list
+    defects[defects_argmax,:,3] = 0
+    defects_argmax = defects[:,:,3].argmax()# now the biggest is bridge between tongue and lips
+
+    s, e, f, d = defects[defects_argmax, 0]
+    bridge_x, bridge_y = tuple(contour[f][0])
+    mask[:, :bridge_x] = 0
+    # cv2.line(mask, (bridge_x - 4, 0), (bridge_x - 4, mask.shape[0] - 1), 0, 8)
+
+    # for i in range(defects.shape[0]):
+    #     s,e,f,d = defects[i, 0]
+    #     print(d/256.0)
+    #     if d/256.0 > 5:
+    #         start = tuple(largest_contour[s][0])
+    #         end = tuple(largest_contour[e][0])
+    #         far = tuple(largest_contour[f][0])
+    #         cv2.line(mask,start,end,127,1)
+    #         cv2.circle(mask,far,2,127,-1)
+
+def remove_chin(mask, contour):
+    hull = cv2.convexHull(contour)
+
+    left_most = tuple(hull[hull[:,:,0].argmin()][0])
+
+    xy_sums = hull[:,:,0] + hull[:,:,1]
+    bottom_right_id = xy_sums.argmax()
+    bottom_right = tuple(hull[bottom_right_id][0])
+
+    cv2.line(mask, left_most, bottom_right, 0, 4)
+
+    # extRight = tuple(c[c[:, :, 0].argmax()][0])
+    # extTop = tuple(c[c[:, :, 1].argmin()][0])
+    # extBot = tuple(c[c[:, :, 1].argmax()][0])
+
+    # hull = cv2.convexHull(contour, returnPoints = False)
+    #
+    # defects = cv2.convexityDefects(contour, hull)
+
+    # defects_argmax = defects[:,:,3].argmax()# remove tongue-arch defect
+    # defects[defects_argmax,:,3] = 0
+    # defects_argmax = defects[:,:,3].argmax()# now the biggest is bridge between tongue and lips
+
+    # s, e, f, d = defects[defects_argmax, 0]
+    # bridge_x, bridge_y = tuple(contour[f][0])
+    # mask[:, :bridge_x] = 0
+    # cv2.line(mask, (bridge_x - 4, 0), (bridge_x - 4, mask.shape[0] - 1), 0, 8)
+
+    # print('start frame')
+    # for i in range(defects.shape[0]):
+    #     s,e,f,d = defects[i, 0]
+    #
+    #     print(d/256.0)
+    #     if (d/256.0 > 5):
+    #         start = tuple(contour[s][0])
+    #         end = tuple(contour[e][0])
+    #         far = tuple(contour[f][0])
+    #         cv2.line(mask,start,end,127,1)
+    #         cv2.circle(mask,far,2,127,-1)
+    # print('end frame')
+    # imshowbig('ibuwv', mask)
+
 def extract_tongue_from(motion, THRESHOLD):
-    viz_motion = motion/motion.max()
-    mask = (viz_motion > THRESHOLD).astype('uint8')
+    mask = (motion/motion.max() > THRESHOLD).astype('uint8')
+
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    largest_contour = find_largest(contours)
+    M = cv2.moments(largest_contour)
+    centroid = (M['m10']/M['m00'], M['m01']/M['m00'])
+    remove_lips(mask, largest_contour)
 
     contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    largest_contour = find_largest(contours)
-    hull = cv2.convexHull(largest_contour)
+    remove_chin(mask, match_centroid(contours, centroid))
+
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    hull = cv2.convexHull(match_centroid(contours, centroid))
 
     mask_tongue = np.zeros_like(mask)
     cv2.drawContours(mask_tongue, [hull], 0, (1), -1)
@@ -30,8 +116,8 @@ def extract_tongue_from(motion, THRESHOLD):
     kernel = np.ones((3, 3), dtype = 'uint8')
     mask_tongue_dilated = cv2.dilate(mask_tongue, kernel, iterations = 1)
 
-    imshowbig('mask', mask*255)
-    imshowbig('mask_tongue', mask_tongue_dilated*255)
-    cv2.waitKey(0)
+    vis = (motion/motion.max()*255).astype('uint8')
+    cv2.drawContours((vis), [hull], 0, (255), 1)
+    imshowbig('mask', vis)
 
     return mask_tongue_dilated.astype('bool')
